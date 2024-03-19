0d3b5f68dd32c67b9e174e129d83bc6067a9a2bf
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-19 16:20:37 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """
         Returns the dictionary of all objects.
         """
