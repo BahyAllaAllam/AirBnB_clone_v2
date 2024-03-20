d1705d27136a0ec8adab9d0bb44ae64b5176c891
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-20 04:12:19 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
     User class inherits from BaseModel.
     Public class attributes:
