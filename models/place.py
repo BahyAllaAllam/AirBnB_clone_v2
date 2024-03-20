c3f888422318ca17ed043309d2d28b74f3fc7347
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-20 08:40:51 -0700

    fix

--- models/place.py
+++ models/place.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Place module"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Place(BaseModel):
@@ -28,14 +31,35 @@ class Place(BaseModel):
         Create a new instance of Place with
             optional parameters to represent different places."""
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    __tablename__ = 'places'
+    if storage_type == 'db':
+        city_id = Column(String(60), nullable=False)
+        user_id = Column(String(60), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        amenity_ids = []
+
+        user = relationship("User", back_populates="places")
+        city = relationship("City", back_populates="places")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Place instance."""
+        super().__init__(*args, **kwargs)
