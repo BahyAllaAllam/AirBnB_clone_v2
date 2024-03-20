03e73cb987d16f79f6fd1c7a91d7058d9066c5be
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-20 04:11:22 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """User module"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
@@ -12,10 +15,27 @@ class User(BaseModel):
         first_name: string - empty string
         last_name: string - empty string
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+            'Place',
+            cascade="all, delete, delete-orphan",
+            backref='user'
+        )
+        reviews = relationship(
+            'Review',
+            cascade="all, delete, delete-orphan",
+            backref='user'
+        )
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize User instance."""
