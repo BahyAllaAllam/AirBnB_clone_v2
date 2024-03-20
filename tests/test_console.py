dda4e43c0111a829748a321a3dcc6ffc5e6088a9
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-20 03:10:47 -0700

    fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -10,35 +10,43 @@ from console import HBNBCommand
 
 
 class TestHBNBCommand(unittest.TestCase):
+    """Unit tests for the console using mock."""
 
     def setUp(self):
+        """Set up the data to test."""
         self.cmd = HBNBCommand()
 
     def tearDown(self):
+        """Destroy the data after testing."""
         pass
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_quit_command(self, mock_stdout):
+        """Test quit command."""
         self.assertTrue(self.cmd.onecmd("quit"))
         self.assertEqual(mock_stdout.getvalue(), '')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_EOF_command(self, mock_stdout):
+        """Test end of file CTRL+D command."""
         self.assertTrue(self.cmd.onecmd("EOF"))
         self.assertEqual(mock_stdout.getvalue(), '\n')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_emptyline(self, mock_stdout):
+        """Test empty line."""
         self.cmd.onecmd("")
         self.assertEqual(mock_stdout.getvalue(), '')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_command(self, mock_stdout):
+        """Test create function."""
         self.cmd.onecmd("create BaseModel")
         self.assertTrue(mock_stdout.getvalue().strip())
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_with_parameters(self, mock_stdout):
+        """Test create with params."""
         self.cmd.onecmd('create State name="California" population=4000000')
         output = mock_stdout.getvalue().strip()
         self.assertTrue(output.startswith("Invalid parameter"))
@@ -56,23 +64,27 @@ class TestHBNBCommand(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_show_command(self, mock_stdout):
+        """Test show function."""
         self.cmd.onecmd("show BaseModel")
         self.assertEqual(
             mock_stdout.getvalue().strip(), "** instance id missing **")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_destroy_command(self, mock_stdout):
+        """Test destroy function."""
         self.cmd.onecmd("destroy BaseModel")
         self.assertEqual(
             mock_stdout.getvalue().strip(), "** instance id missing **")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_all_command(self, mock_stdout):
+        """Test all function."""
         self.cmd.onecmd("all")
         self.assertTrue(mock_stdout.getvalue().strip())
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_update_command(self, mock_stdout):
+        """Test update function."""
         self.cmd.onecmd("update BaseModel 1234 name John")
         self.assertEqual(
             mock_stdout.getvalue().strip(), "** no instance found **")
