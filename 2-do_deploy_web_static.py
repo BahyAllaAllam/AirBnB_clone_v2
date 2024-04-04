a8bdbae9341633a8d9add6f294666ff0cb9a46d8
BahyAllaAllam <bahy.alam@yahoo.com>
2024-04-04 13:08:56 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,28 +4,26 @@
    using the function do_deploy."""
 
 from pathlib import Path
-from fabric.api import env, put, run
+from fabric.api import *
+from os.path import exists
 env.hosts = ['3.84.168.105', '100.26.18.236']
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web servers."""
-    if not Path(archive_path).is_file():
+    if not exists(archive_path):
         return False
 
     file_name = archive_path.split('/')[1]
-    file_path = '/data/web_static/releases/'
-    releases_path = file_path + file_name[:-4]
+    file_path = '/data/web_static/releases/' + "{}".format(file_name.split('.')[0])
     try:
         put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(releases_path))
-        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
-        run('rm -rf /tmp/{}'.format(file_name))
-        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
-        run('rm -rf {}/web_static'.format(releases_path))
+        run('mkdir -p {}/'.format(file_path))
+        run('rm -r /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(file_path, file_path))
+        run('rm -rf {}/web_static'.format(file_path))
         run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(releases_path))
-        print('New version deployed!')
+        run('ln -s {}/ /data/web_static/current'.format(file_path))
         return True
     except Exception:
         return False
