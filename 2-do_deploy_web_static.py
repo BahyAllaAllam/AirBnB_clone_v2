40712fb78d88f99cb8f441d04f26504ed9a713a9
BahyAllaAllam <bahy.alam@yahoo.com>
2024-04-04 19:13:26 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,22 +13,22 @@ env.hosts = ['3.84.168.105', '100.26.18.236']
 
 def do_deploy(archive_path):
     """Distributes an archive to a web servers."""
-    if not Path(archive_path).is_file():
+    if not exists(archive_path):
         return False
 
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        put(archive_path, "/tmp/")
-        archive_filename = Path(archive_path).stem
-        release_folder = "/data/web_static/releases/{}".format(
-                         archive_filename)
-        run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{}.tgz -C {}".format(
-            archive_filename, release_folder))
-        run('rm -r /tmp/{}.tgz'.format(archive_filename))
-        current_link = "/data/web_static/current"
-        run("rm -rf {}".format(current_link))
-        run("ln -s {} {}".format(release_folder, current_link))
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm -rf /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-    except Exception:
+    except Exception as e:
         return False
